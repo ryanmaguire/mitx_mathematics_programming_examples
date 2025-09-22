6b2331ea34ef301e2ab1d72fc60ccab2c591b8cd
ryanmaguire <rmaguire@protonmail.com>
2025-09-21 22:27:41 -0400

    Adding more Newton and Halley functions.

---
+++ electromagnetism/electromagnetic_radiation/heaviside_feynman_radiation_C89/halley_error.c
@@ -0,0 +1,65 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of mitx_mathematics_programming_examples.               *
+ *                                                                            *
+ *  mitx_mathematics_programming_examples is free software: you can           *
+ *  redistribute it and/or modify it under the terms of the GNU General       *
+ *  Public License as published by the Free Software Foundation, either       *
+ *  version 3 of the License, or (at your option) any later version.          *
+ *                                                                            *
+ *  mitx_mathematics_programming_examples is distributed in the hope that     *
+ *  it will be useful but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.          *
+ *  See the GNU General Public License for more details.                      *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with mitx_mathematics_programming_examples. If not, see             *
+ *  <https://www.gnu.org/licenses/>.                                          *
+ ******************************************************************************
+ *  Function:                                                                 *
+ *      halley_error                                                          *
+ *  Purpose:                                                                  *
+ *      This function tests whether or not Halley's method converged after a  *
+ *      few iterations. If it did, the output of the past_halley function is  *
+ *      the actual retarded time for the moving charge. If it did not, we     *
+ *      have an error and need to correct this.                               *
+ *  Arguments:                                                                *
+ *      rho (double):                                                         *
+ *          The distance from the point to the origin.                        *
+ *      time (double):                                                        *
+ *          The time for the point in the plane.                              *
+ *  Output:                                                                   *
+ *      error (double):                                                       *
+ *          The error in Halley's method. Ideally, this is close to zero.     *
+ *  Called Functions:                                                         *
+ *      heaviside_feynman.h:                                                  *
+ *          halley:                                                           *
+ *              Computes the Halley iterate for the retarded time.            *
+ *  Method:                                                                   *
+ *      Compute t_r - t + distance / c (but we set c = 1, for simplicity).    *
+ ******************************************************************************
+ *  Author: Ryan Maguire                                                      *
+ *  Date:   2025/09/20                                                        *
+ ******************************************************************************/
+
+/*  Function prototype / forward declaration provided here.                   */
+#include "heaviside_feynman.h"
+
+/*  Function for computing the error in Halley's method.                      */
+double halley_error(double rho, double time, unsigned int iters)
+{
+    /*  Use Halley's method to compute the retarded time.                     */
+    const double retarded_time = past_halley(rho, time, iters);
+
+    /*  Now that we have a guess for the retarded time for the oscillating    *
+     *  point charge, we may use this to compute the distance between the     *
+     *  point rho in the xy plane and the position of the oscillating charge  *
+     *  at the retarded time.                                                 */
+    const double dist = distance(rho, retarded_time);
+
+    /*  Retarded time is tr = t - (1 / c) || r(tr) - rho(t) ||. If Halley's   *
+     *  method converged, the following expression should be close to zero.   */
+    return retarded_time - time + dist;
+}
+/*  End of halley_error.                                                      */
---
+++ electromagnetism/electromagnetic_radiation/heaviside_feynman_radiation_C89/past_newton.c
@@ -0,0 +1,68 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of mitx_mathematics_programming_examples.               *
+ *                                                                            *
+ *  mitx_mathematics_programming_examples is free software: you can           *
+ *  redistribute it and/or modify it under the terms of the GNU General       *
+ *  Public License as published by the Free Software Foundation, either       *
+ *  version 3 of the License, or (at your option) any later version.          *
+ *                                                                            *
+ *  mitx_mathematics_programming_examples is distributed in the hope that     *
+ *  it will be useful but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.          *
+ *  See the GNU General Public License for more details.                      *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with mitx_mathematics_programming_examples. If not, see             *
+ *  <https://www.gnu.org/licenses/>.                                          *
+ ******************************************************************************
+ *  Function:                                                                 *
+ *      past_newton                                                           *
+ *  Purpose:                                                                  *
+ *      Given a point in the xy plane and the time at this point, this        *
+ *      computes the retarded time for a charge oscillating in the z axis by  *
+ *      iteratively applying Newton's method.                                 *
+ *  Arguments:                                                                *
+ *      rho (double):                                                         *
+ *          The distance from the point to the origin.                        *
+ *      time (double):                                                        *
+ *          The time for the point in the plane.                              *
+ *      iters (unsigned int):                                                 *
+ *          The number of iterations that will be performed.                  *
+ *  Output:                                                                   *
+ *      retarded_time (double):                                               *
+ *          The time for the oscillating charge.                              *
+ *  Called Functions:                                                         *
+ *      heaviside_feynman.h:                                                  *
+ *          newton:                                                           *
+ *              Computes the Newton iterate for the retarded time.            *
+ *  Method:                                                                   *
+ *      Iteratively apply Newton's method with the guess t0 = t - rho.        *
+ ******************************************************************************
+ *  Author: Ryan Maguire                                                      *
+ *  Date:   2025/09/20                                                        *
+ ******************************************************************************/
+
+/*  Function prototype / forward declaration provided here.                   */
+#include "heaviside_feynman.h"
+
+/*  Function for computing the retarded time for an oscillating charge.       */
+double past_newton(double rho, double time, unsigned int iters)
+{
+    /*  The charge is oscillating in the z axis between -1 and 1. The time it *
+     *  takes for light to travel from the origin to the point is a good      *
+     *  approximation to the actual light travel time.                        */
+    double tn = time - rho;
+
+    /*  Variable for indexing over the loop for Newton's method.              */
+    unsigned int ind;
+
+    /*  Loop through and iteratively apply Newton's method.                   */
+    for (ind = 0; ind < iters; ++ind)
+        tn = newton(rho, time, tn);
+
+    /*  If iters is large enough, we have a very good approximation.          */
+    return tn;
+}
+/*  End of past_newton.                                                       */
